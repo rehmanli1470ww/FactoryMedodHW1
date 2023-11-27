23a509fc775b6ce7ccf01a98e0596bbd0e343e3b
rehmanli1470ww <144922823+rehmanli1470ww@users.noreply.github.com>
2023-11-27 16:48:57 +0400

    Add project files.

---
+++ FactoryMedodHW1/Program.cs
@@ -0,0 +1,62 @@
+﻿
+
+interface Button
+{
+    public void render();
+    public void onClick();
+
+}
+
+class Windows : Button
+{
+    public void onClick()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void render()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class HTML : Button
+{
+    public void onClick()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void render()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+abstract class Dialog
+{
+    public void render() { }
+    public abstract Button createButton();  
+
+}
+
+class WindowsDialog : Dialog
+{
+    public override Button createButton()
+    {
+        throw new NotImplementedException();
+    }
+    
+}
+
+class WebDialog : Dialog
+{
+    public override Button createButton()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+
